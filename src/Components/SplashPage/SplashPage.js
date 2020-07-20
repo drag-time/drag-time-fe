7c4d0651cf9cd89bac53b6676e33eeea5f067def
collinkallery <collin.kallery@colorado.edu>
2020-07-20 16:55:38 -0600

    Basic styling of splash page with colors and fonts

--- src/Components/SplashPage/SplashPage.js
+++ src/Components/SplashPage/SplashPage.js
@@ -9,14 +9,46 @@ const Wrapper = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: #57123A;
+
+  h1 {
+    font-family: 'MuseoModerno', cursive;
+    font-weight: 100;
+    font-size: 6em;
+    color: white;
+  }
+  #time {
+    font-weight: 500;
+    color: #2299A3;
+  }
 `;
+
+const NavLinkStyled = styled(NavLink)`
+  font-family: 'Raleway', sans-serif;
+  font-size: 1.6em;
+  text-decoration: none;
+  border: none;
+  background-color: #A39743;
+  color: white;
+  padding: 1% 5%;
+  border-radius: 15px;
+  margin: 1%;
+
+  :hover {
+    cursor: pointer;
+    outline-offset: 15px;
+    text-shadow: 1px 1px 2px #427388;
+    box-shadow: inset 0 0 40px #A39743, 0 0 40px #A39743;
+    outline-color: #A39743;
+  }
+`
 const SplashPage = () => {
   return (
     <Wrapper>
       <h1>
-        Welcome to Drag Time!
+        DRAG<span id="time">TIME</span>
       </h1>
-      <NavLink to='/home'>Go to my events</NavLink>
+      <NavLinkStyled to='/home'>Enter</NavLinkStyled>
     </Wrapper>
   );
 }
