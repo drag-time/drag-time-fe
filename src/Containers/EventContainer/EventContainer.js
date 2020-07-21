4d515b8e7d223934700d03164ac561871aed6d50
collinkallery <collin.kallery@colorado.edu>
2020-07-21 16:39:43 -0600

    Minor changes to pre-styling

--- src/Containers/EventContainer/EventContainer.js
+++ src/Containers/EventContainer/EventContainer.js
@@ -1,8 +1,28 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import Header from '../../Components/Header/Header.js';
+import SearchForm from '../SearchForm/SearchForm.js'
 import Event from '../Event/Event.js';
 import { connect } from 'react-redux';
 import { getEvents } from '../../Actions';
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+const BodyWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`
+
+const EventWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`
 
 const EventContainer = (props) => {
 
@@ -15,7 +35,7 @@ const EventContainer = (props) => {
         <Event key={event.id} {...event} />
       )
     })
-    return eventsToDisplay
+    return eventsToDisplay;
   }
 
   useEffect(() => {
@@ -23,11 +43,16 @@ const EventContainer = (props) => {
   })
 
   return(
-    <section>
+    <Wrapper>
       <Header />
-      <h2>Event Feed</h2>
-      {!!props.eventList && displayEvents()}
-    </section>
+      <BodyWrapper>
+        <SearchForm />
+        <EventWrapper>
+          <h2>Event Feed</h2>
+          {!!props.eventList && displayEvents()}
+        </EventWrapper>
+      </BodyWrapper>
+    </Wrapper>
   );
 }
 
