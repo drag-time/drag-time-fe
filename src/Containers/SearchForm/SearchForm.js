809471ab49d6f4833c69a8cca0842ef1da1bcd1d
collinkallery <collin.kallery@colorado.edu>
2020-07-26 09:16:36 -0600

    Change search form into class, add methods to track input

--- src/Containers/SearchForm/SearchForm.js
+++ src/Containers/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.section`
@@ -7,7 +7,7 @@ const Wrapper = styled.section`
   width: 20%;
   padding: 1%;
   background-color: #57123A;
-  height: 90%;
+  height: 100%;
   border-right: 1px solid white;
 
   input {
@@ -43,20 +43,19 @@ const Wrapper = styled.section`
     flex-direction: row;
     align-items: center;
     padding: 3%;
-    cursor: pointer;
     font-size: 1.1em;
     font-family: 'Raleway', sans-serif;
     color: white;
     border-bottom: 1px solid white;
     margin: 2%;
 
-    :hover input ~ span {
+    /* :hover input ~ span {
       background-color: #A39743;
       outline-offset: 15px;
       text-shadow: 1px 1px 2px #427388;
       box-shadow: inset 0 0 40px #A39743, 0 0 40px #A39743;
       outline-color: #A39743;
-    }
+    } */
 
     input {
       opacity: 0;
@@ -75,6 +74,7 @@ const Wrapper = styled.section`
 
     span {
       height: 22px;
+      cursor: pointer;
       width: 22px;
       background-color: #2299A3;
       margin-left: auto;
@@ -96,120 +96,102 @@ const Wrapper = styled.section`
   }
 `
 
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: '',
+      eighteenPlus: false,
+      twentyOnePlus: false,
+      coverCharge: false,
+      dragKing: false,
+      dragQueen: false,
+      ballroom: false,
+      pageant: false,
+      trivia: false,
+      musical: false,
+      tribute: false,
+      horror: false
+    }
+  }
 
-const SearchForm = (props) => {
+  trackKeyword = (e) => {
+    this.setState({
+      keyword: e.target.value
+    });
+  };
 
-  return (
-    <Wrapper>
-      <input type="text" placeholder="Search by keyword..." />
-      <label>18+
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>21+
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Cover Charge
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Drag King
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Drag Queen
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Ballroom
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Pageant
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Trivia
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Musical
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Tribute
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <label>Horror
-        <input type="checkbox" />
-        <span></span>
-      </label>
-      <button>Submit Search</button>
-    </Wrapper>
-  )
+  trackLabels = (e) => {
+    const label = e.target.id;
+    if (!this.state[label]) {
+      this.setState({
+        [label]: true
+      })
+    } else {
+      this.setState({
+        [label]: false
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <form>
+          <input onChange={this.trackKeyword} type="text" placeholder="Search by keyword..." />
+          <label>18+
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="eighteenPlus"></span>
+          </label>
+          <label>21+
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="twentyOnePlus"></span>
+          </label>
+          <label>Cover Charge
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="coverCharge"></span>
+          </label>
+          <label>Drag King
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="dragKing"></span>
+          </label>
+          <label>Drag Queen
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="dragQueen"></span>
+          </label>
+          <label>Ballroom
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="ballroom"></span>
+          </label>
+          <label>Pageant
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="pageant"></span>
+          </label>
+          <label>Trivia
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="trivia"></span>
+          </label>
+          <label>Musical
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="musical"></span>
+          </label>
+          <label>Tribute
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="tribute"></span>
+          </label>
+          <label>Horror
+            <input type="checkbox" />
+            <span onClick={this.trackLabels} id="horror"></span>
+          </label>
+          <button>Submit Search</button>
+        </form>
+      </Wrapper>
+    )
+  }
 }
 
 export default SearchForm;
-
-//
-// <TagWrapper>
-//   <input type="checkbox" id="18+" name="18+" value="18+" />
-//   <label for="18+">18+</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="21+" name="21+" value="21+" />
-//   <label for="21+">21+</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="cover-charge" name="cover-charge" value="cover-charge" />
-//   <label for="cover-charge">Cover Charge</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="drag-king" name="drag-king" value="drag-king" />
-//   <label for="drag-king">Drag King</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="drag-queen" name="drag-queen" value="drag-queen" />
-//   <label for="drag-queen">Drag Queen</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="ballroom" name="ballroom" value="ballroom" />
-//   <label for="ballroom">Ballroom</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="pageant" name="pageant" value="pageant" />
-//   <label for="pageant">Pageant</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="trivia" name="trivia" value="trivia" />
-//   <label for="trivia">Trivia</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="musical" name="musical" value="musical" />
-//   <label for="musical">Musical</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="horror" name="horror" value="horror" />
-//   <label for="horror">Horror</label>
-// </TagWrapper>
-// <TagWrapper>
-//   <input type="checkbox" id="tribute" name="tribute" value="tribute" />
-//   <label for="tribute">Tribute</label>
-// </TagWrapper>
-
-//
-// const TagWrapper = styled.section`
-//   display: flex;
-//   flex-direction: row;
-//   padding: 2%;
-//   align-items: center;
-//
-//   label {
-//     font-family: 'Raleway', sans-serif;
-//     font-size: 1.3em;
-//   }
-//   input {
-//
-//   }
